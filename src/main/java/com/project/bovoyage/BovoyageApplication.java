8c890f0933d90cc458052d9ff099b2ca8225fff4
manal <manal.salmi1501@gmail.com>
2021-05-03 01:11:28 +0200

    first commit

---
+++ src/main/java/com/project/bovoyage/BovoyageApplication.java
@@ -0,0 +1,13 @@
+package com.project.bovoyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BovoyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BovoyageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bovoyage/BovoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bovoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BovoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
